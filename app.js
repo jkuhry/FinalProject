f880041769333a441ff6b73821102982b2163528
Jack Kuhry <jackkuhry@Jacks-MacBook-Air.local>
2016-08-04 19:06:09 -0700

    FirstTarget

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/form',function(req,res){
+  res.sendFile(path.join(__dirname+'/form.html'));
+});
+
+app.get('/search',function(req,res){
+  res.sendFile(path.join(__dirname+'/search.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
